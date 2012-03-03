c5e106f3eb05ef85136ac8393f16314d89dd8811
Hybrid0 <creavol@gmail.com>
2012-03-03 12:24:32 +0900

    GIRayCamera added.

--- GIEngineCore/Camera.cpp
+++ GIEngineCore/Camera.cpp
@@ -20,3 +20,56 @@ GICamera::GICamera()
 GICamera::~GICamera()
 {
 }
+
+GIRayCamera::GIRayCamera()
+	: m_RayCount(0), m_RayArray(NULL) //m_bRaysDirty(false)
+{
+	
+}
+
+GIRayCamera::~GIRayCamera()
+{
+	SafeDeleteArray( &m_RayArray );
+}
+
+void GIRayCamera::CreateRays()
+{
+	// TODO: 잘못된 width, height 값(0 혹은 쓰레기값)에 대한 대비가 없음.
+	assert( Width != 0 && Height != 0 );
+
+	m_RayCount = Width * Height;
+	if( m_RayArray == NULL )
+		m_RayArray = new GIRay[m_RayCount];
+
+	float halfHeight = tan( TO_RADIAN( GetFovy() ) );
+	float halfWidth = halfHeight * GetAspectRatio();
+	GIVector3 LeftTop = GetOrigin() + GetDirection() + GetUpDirection() * halfHeight + GetRightDirection() * -halfWidth;
+
+	float fWidthPixel = float(Width);
+	float fHeightPixel = float(Height);
+
+	for( unsigned int y = 0; y < Height; y++ )
+		for( unsigned int x = 0; x < Width; x++ )
+		{
+			GIVector3 dest = LeftTop + GetDirection() * float(y)/fWidthPixel+ GetRightDirection() * float(x)/fHeightPixel;
+
+			GIRay &ray = m_RayArray[x+y*Width];
+			GIVector3 direction = (dest - GetOrigin()).GetNormalized();
+			ray.dir << direction;
+			ray.org << GetOrigin();
+			ray.NearDistance = GetNearDistance();
+			ray.FarDistance = GetFarDistance();
+		}
+
+	m_bRaysDirty = false;
+}
+
+unsigned int GIRayCamera::GetCameraRayCount()
+{
+	return m_RayCount;
+}
+
+GIRay* GIRayCamera::GetCameraRays()
+{
+	return m_RayArray;
+}
--- GIEngineCore/Camera.h
+++ GIEngineCore/Camera.h
@@ -17,6 +17,26 @@ public:
 	GICamera();
 	virtual ~GICamera();
 
+	GIVector3 GetOrigin() { return Origin; }
+	GIVector3 GetDirection() { return Direction; }
+	GIVector3 GetRightDirection() { return Direction.CrossProduct( UpDirection ); }
+	GIVector3 GetUpDirection() { return UpDirection; }
+
+	float GetFovy() { return Fovy; }
+	// TODO: º¯¼ö·Î
+	float GetAspectRatio() { return (float)Width/(float)Height; }
+
+	float GetNearDistance() { return NearDistance; }
+	float GetFarDistance() { return FarDistance; }
+
+// const version
+	const GIVector3& GetOrigin() const { return Origin; }
+	const GIVector3& GetDirection() const { return Direction; }
+	GIVector3 GetRightDirection() const { return Direction.CrossProduct( UpDirection ); }
+	const GIVector3& GetUpDirection() const { return UpDirection; }
+
+	// TODO: get/set, private
+//private:
 	GIVector3 Origin;
 	GIVector3 Direction;
 	GIVector3 UpDirection;
@@ -28,5 +48,22 @@ public:
 	// Default : [0, \inf)
 	float NearDistance;
 	float FarDistance;
+
+};
+
+class GIRayCamera : public GICamera
+{
+public:
+	GIRayCamera();
+	virtual ~GIRayCamera();
+
+	void CreateRays();
+	unsigned int GetCameraRayCount();
+	GIRay* GetCameraRays();
+
+private:
+	unsigned int m_RayCount;
+	GIRay *m_RayArray;
+	bool m_bRaysDirty;
 };
 };
